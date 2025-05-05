7e22bc3a69810b13137ae808a1535264ff1a568b
gilmore606 <dan.burford@gmail.com>
2025-05-05 11:44:20 -0700

    reduce constants

--- src/main/kotlin/compiler/ast/N_BINOP.kt
+++ src/main/kotlin/compiler/ast/N_BINOP.kt
@@ -1,6 +1,8 @@
 package com.dlfsystems.compiler.ast
 
 import com.dlfsystems.compiler.Coder
+import com.dlfsystems.value.VVoid
+import com.dlfsystems.value.Value
 import com.dlfsystems.vm.Opcode
 import com.dlfsystems.vm.Opcode.*
 
@@ -9,20 +11,42 @@ import com.dlfsystems.vm.Opcode.*
 abstract class N_BINOP(val opString: String, val left: N_EXPR, val right: N_EXPR, val ops: List<Opcode>): N_EXPR() {
     override fun toText() = "($left $opString $right)"
     override fun kids() = listOf(left, right)
+    override fun constantValue(): Value? {
+        left.constantValue()?.also { leftConstant ->
+            right.constantValue()?.also { rightConstant ->
+                asConstant(leftConstant, rightConstant)?.also { return it }
+            }
+        }
+        return null
+    }
+    open fun asConstant(l: Value, r: Value): Value? = null
 
     override fun code(coder: Coder) {
+        if (codeConstant(coder)) return
         left.code(coder)
         right.code(coder)
         ops.forEach { coder.code(this, it) }
     }
 }
 
-class N_ADD(left: N_EXPR, right: N_EXPR): N_BINOP("+", left, right, listOf(O_ADD))
-class N_SUBTRACT(left: N_EXPR, right: N_EXPR): N_BINOP("-", left, right, listOf(O_NEGATE, O_ADD))
-class N_MULTIPLY(left: N_EXPR, right: N_EXPR): N_BINOP("*", left, right, listOf(O_MULT))
-class N_DIVIDE(left: N_EXPR, right: N_EXPR): N_BINOP("/", left, right, listOf(O_DIV))
-class N_POWER(left: N_EXPR, right: N_EXPR): N_BINOP("^", left, right, listOf(O_POWER))
-class N_MODULUS(left: N_EXPR, right: N_EXPR): N_BINOP("%", left, right, listOf(O_MODULUS))
+class N_ADD(left: N_EXPR, right: N_EXPR): N_BINOP("+", left, right, listOf(O_ADD)) {
+    override fun asConstant(l: Value, r: Value) = l.plus(r)
+}
+class N_SUBTRACT(left: N_EXPR, right: N_EXPR): N_BINOP("-", left, right, listOf(O_NEGATE, O_ADD)) {
+    override fun asConstant(l: Value, r: Value) = l.plus(r.negate() ?: VVoid)
+}
+class N_MULTIPLY(left: N_EXPR, right: N_EXPR): N_BINOP("*", left, right, listOf(O_MULT))  {
+    override fun asConstant(l: Value, r: Value) = l.multiply(r)
+}
+class N_DIVIDE(left: N_EXPR, right: N_EXPR): N_BINOP("/", left, right, listOf(O_DIV)) {
+    override fun asConstant(l: Value, r: Value) = l.divide(r)
+}
+class N_POWER(left: N_EXPR, right: N_EXPR): N_BINOP("^", left, right, listOf(O_POWER)) {
+    override fun asConstant(l: Value, r: Value) = l.toPower(r)
+}
+class N_MODULUS(left: N_EXPR, right: N_EXPR): N_BINOP("%", left, right, listOf(O_MODULUS)) {
+    override fun asConstant(l: Value, r: Value) = l.modulo(r)
+}
 class N_AND(left: N_EXPR, right: N_EXPR): N_BINOP("&&", left, right, listOf(O_AND))
 class N_OR(left: N_EXPR, right: N_EXPR): N_BINOP("||", left, right, listOf(O_OR))
 class N_IN(left: N_EXPR, right: N_EXPR): N_BINOP("in", left, right, listOf(O_IN))
