3562d7fb5beac225fbdf1ff8b45757f7b173e84f
gilmore606 <dan.burford@gmail.com>
2025-06-05 19:01:17 -0700

    basic verb tests

---
+++ src/test/kotlin/VerbTest.kt
@@ -0,0 +1,62 @@
+package com.dlfsystems
+
+import kotlin.test.Test
+
+class VerbTest: YeggTest() {
+
+    @Test
+    fun `Verbs on objects with traits`() = yeggTest {
+        run($$"""
+            createTrait("animal")
+            createTrait("dog")
+            addParent($dog, $animal)
+        """)
+
+        verb("animal", "getNoise", "return \"generic noise\"")
+        verb("dog", "getNoise", "return \"bark\"")
+
+        runForOutput($$"""
+            o = create($dog)
+            notifyConn("Dog says: ${o.getNoise()}!")
+            a = create($animal)
+            notifyConn("Animal says: ${a.getNoise()}!")
+        """, """
+            Dog says: bark!
+            Animal says: generic noise!
+        """)
+    }
+
+    @Test
+    fun `Verbs calling other verbs`() = yeggTest {
+        verb("sys", "test", $$"""
+            [input] = args
+            notifyConn("test for $input")
+            return 5 + $sys.test2(input)
+        """)
+        verb("sys", "test2", $$"""
+            [input] = args
+            notifyConn("test2 for $input")
+            return 3 + $sys.test3(input)
+        """)
+        verb("sys", "test3", $$"""
+            [input] = args
+            notifyConn("test3 for $input")
+            return 1 + input
+        """)
+
+        runForOutput($$"""
+            notifyConn("For 1 : ${$sys.test(1)}")
+            notifyConn("For 10: ${$sys.test(10)}")
+        """, """
+            test for 1
+            test2 for 1
+            test3 for 1
+            For 1 : 10
+            test for 10
+            test2 for 10
+            test3 for 10
+            For 10: 19
+        """)
+    }
+
+}
