1fa2e30f3127bb6699f12c8d7914536d2826ebac
gilmore606 <dan.burford@gmail.com>
2025-08-28 23:44:12 -0700

    better split panes

--- yeggide/src/App.tsx
+++ yeggide/src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react'
 import CodeEditor from "./components/CodeEditor";
-import SplitPane from "react-split-pane";
+import "allotment/dist/style.css"
+import './index.css'
+import './App.css'
+import {Allotment} from "allotment";
 
 class App extends React.Component<any, any> {
 
   render() {
     return (
-        <SplitPane split="vertical" className="ideContainer" defaultSize={150} minSize={50} maxSize={400} allowResize={true}>
-            <div className="projectPane pane">
-                PROJECT
-            </div>
-            <SplitPane split="horizontal" defaultSize={50} allowResize={false}>
-                <div className="tabsPane pane">
-                    tabs
-                </div>
-                <div className="editorPane pane">
-                    <CodeEditor />
-                </div>
-            </SplitPane>
-        </SplitPane>
+        <div style={{ width: "100vw", height: "100vh" }}>
+            <Allotment>
+                <Allotment.Pane preferredSize={150} maxSize={400} minSize={50}>
+                    <div className="projectPane pane">
+                        PROJECT
+                    </div>
+                </Allotment.Pane>
+                <Allotment.Pane>
+                    <Allotment vertical>
+                        <Allotment.Pane preferredSize={60} maxSize={60} minSize={60}>
+                            <div className="tabsPane pane">
+                                tabs
+                            </div>
+                        </Allotment.Pane>
+                        <Allotment.Pane>
+                            <div className="editorPane pane">
+                                <CodeEditor />
+                            </div>
+                        </Allotment.Pane>
+                    </Allotment>
+                </Allotment.Pane>
+            </Allotment>
+        </div>
     )
   }
 }
