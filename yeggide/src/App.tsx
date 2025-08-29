9bd8856f7264628fe5f96bd74f10ee8ff797bd1e
gilmore606 <dan.burford@gmail.com>
2025-08-28 21:31:24 -0700

    split panes

--- yeggide/src/App.tsx
+++ yeggide/src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import CodeEditor from "./components/CodeEditor";
+import SplitPane from "react-split-pane";
 
 class App extends React.Component<any, any> {
 
   render() {
     return (
-        <div className="ideContainer">
-          <div className="projectPane">
-            PROJECT
-          </div>
-          <div className="editorPane">
-            EDIT
-            <CodeEditor />
-          </div>
-        </div>
+        <SplitPane split="vertical" className="ideContainer" defaultSize={150} minSize={50} maxSize={400} allowResize={true}>
+            <div className="projectPane pane">
+                PROJECT
+            </div>
+            <SplitPane split="horizontal" defaultSize={50} allowResize={false}>
+                <div className="tabsPane pane">
+                    tabs
+                </div>
+                <div className="editorPane pane">
+                    <CodeEditor />
+                </div>
+            </SplitPane>
+        </SplitPane>
     )
   }
 }
